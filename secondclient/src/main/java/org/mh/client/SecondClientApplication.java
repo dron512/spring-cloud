0a09948f6414e94756ff21aeb0340e0dde090fac
DESKTOP-SCJ83VA\user <parkmyounghoi@gmail.com>
2024-10-08 18:00:02 +0900

    no message

--- secondclient/src/main/java/org/mh/client/SecondClientApplication.java
+++ secondclient/src/main/java/org/mh/client/SecondClientApplication.java
@@ -2,10 +2,10 @@ package org.mh.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class SecondClientApplication {
 
     public static void main(String[] args) {
